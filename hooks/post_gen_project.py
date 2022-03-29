e8b2260b05fb7aed9361b0120d32513250c24b90
Juan Camilo Ceron <juancamiloceron@gmail.com>
2022-03-29 15:12:24 +0200

    Updated post gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,5 +3,5 @@ import shutil
 consume_http_apis = "{{cookiecutter.consume_http_apis}}"
 if consume_http_apis.lower() != "yes":
     shutil.rmtree("./core/clients", ignore_errors=True)
-    # shutil.rmtree("./core/test/test_unittest/test_clients", ignore_errors=True)
-    # shutil.rmtree("./core/test/test_integration/test_clients", ignore_errors=True)
+    shutil.rmtree("./core/tests/tests_unittest/test_clients", ignore_errors=True)
+    shutil.rmtree("./core/tests/tests_integration/test_clients", ignore_errors=True)
